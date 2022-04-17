3d93ea6df8144df8bfa49bffe80bd0084e504453
arpan-svci <arpanmandal913@gmail.com>
2022-04-17 20:21:36 +0530

    fourth commit

--- final/parsing_table_generation.cpp
+++ final/parsing_table_generation.cpp
@@ -29,6 +29,15 @@ class prod_lr0{
         prod_lr0(){
             left_of_production=-1;
         }
+        prod_lr0(const prod_lr0 &p){
+            nonterminal=p.nonterminal;
+            for(int i=0;i<p.length;i++){
+                rule[i]=p.rule[i];
+            }
+            length=p.length;
+            left_of_production=p.left_of_production;
+
+        }
         int getnonterminal(){
             return nonterminal;
         }
@@ -70,6 +79,12 @@ class lr0item{
         lr0item(){
             length=0;
         }
+        lr0item(const lr0item &l){
+            for(int i=0;i<l.length;i++){
+                rules[i]=l.rules[i];
+            }
+            length=l.length;
+        }
         prod_lr0* getproduction(){
             return rules;
         }
@@ -107,10 +122,10 @@ lr0itemset lr0generation(production* productions,int length){
         int nonterminals[100],terminals[100],length_nonterminals=0,length_terminals=0;
         prod_lr0 items[100];
         int stratsymbol[]={200};
-        items[0].getdata(500,stratsymbol,1);
+        items[0].getdata(500,stratsymbol,1); //it is for the first symbol ir S'->S where S is the start symbol
         for(int i=1;i<length+1;i++){
             items[i].getdata(productions[i].getnonterminal(),productions[i].getrules(),productions[i].getlength());
-            addsymbol(&nonterminals[0],&length_nonterminals,productions[i].getnonterminal());
+            addsymbol(&nonterminals[0],&length_nonterminals,productions[i].getnonterminal());               
         }
         lr0item itemlist[50];
         int length_of_itemlist=0;
